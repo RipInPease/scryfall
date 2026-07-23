3670827d6c2bb8ec47be3813822d5448b7614a67
Snorre Vandbakk Fjelde <snorre.vandbakk.fjelde@oneco.no>
2026-07-23 12:58:47 +0200

    Why did I start this fucking project?

--- src/derialize/mod.rs
+++ src/derialize/mod.rs
@@ -15,6 +15,7 @@ pub enum DesValue {
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
     ExpectedToken(char),
+    UnexpectedTopen(char)
 }
 
 /// Will try to parse an object.
@@ -28,52 +29,89 @@ pub fn parse_string(s: String) -> Result<DesValue, ParseError> {
 /// If there was no error, will return [`DesValue::Object`]
 fn parse_object(s: &str, i: &mut usize) -> Result<DesValue, ParseError> {
     let mut i = 0;
-    if &s[0..1] == "{" {
-        return Err(ParseError::ExpectedToken('{'));
-    }
-    
-    let mut fields: Vec<(String, DesValue)> = Vec::new();
-    return Err(ParseError::ExpectedToken('{'));
+    let mut chars = s[i..].char_indices();
+
+    Err(ParseError::ExpectedToken('{'))
 }
 
-/// Reads a Field, Value pair
+/// Reads a Field name
 fn read_field_name(s: &str, i: &mut usize) -> Result<String, ParseError> {
-    while &s[*i..*i+1] != "\"" {
-        *i += 1;
+    let mut chars = s[*i..].char_indices();
 
-        if *i >= s.len()  {
-            return Err(ParseError::ExpectedToken('\"'));
+    // Find the opening quote
+    loop {
+        match chars.next() {
+            None              => return Err(ParseError::ExpectedToken('\"')),
+
+            Some((offset, c)) => {
+                if !c.is_whitespace() && c != '\"' {
+                    return Err(ParseError::UnexpectedTopen(c))
+                } else if c == '\"' {
+                    *i += offset + c.len_utf8();
+                    break;
+                }
+            }
         }
     }
-    *i += 1;
 
-    let field_name = read_string_in_quotes(s, i)?;
-    if *i >= s.len() || &s[*i..*i+1] != "\"" {
-        return Err(ParseError::ExpectedToken('\"'));
-    }
-
-    *i += 1;
-    Ok(field_name)
+    // Read the string inside the quotes
+    read_string_in_quotes(s, i)
 }
 
 // Assumes you are already inside quotes
 fn read_string_in_quotes(s: &str, i: &mut usize) -> Result<String, ParseError> {
-    let mut end = *i;
+    let mut chars = s[*i..].char_indices();
 
-    while &s[end..end+1] != "\"" {
-        end += 1;
-        
-        if end >= s.len()  {
-            return Err(ParseError::ExpectedToken('\"'));
+    loop {
+        match chars.next() {
+            None              => return Err(ParseError::ExpectedToken('\"')),
+            Some((offset, c)) => {
+                if c == '\"' {
+                    let s = s[*i..*i+offset].to_string();
+                    *i += offset + c.len_utf8();
+                    return Ok(s)
+                }
+            }
         }
     }
 
-    let res: String = s[*i..end].to_string();
-    *i = end;
-    Ok(res)
+
+    Err(ParseError::ExpectedToken('\"'))
 }
 
-/// Reads a numerical string not enclosed in quotes
-fn read_string_num(s: &str, i: &mut usize) -> Result<String, ParseError> {
-    Err(ParseError::ExpectedToken(','))
+/// Reads a string not enclosed in quotes. This reads until first whitespace after chars, '}' or ','
+fn read_string_out_quotes(s: &str, i: &mut usize) -> Result<String, ParseError> {
+    let mut chars = s[*i..].char_indices();
+    let mut first_char = false;
+    let mut start = *i;
+
+    loop {
+        match chars.next() {
+            None => {
+                let res = s[start..].to_string();
+                *i = start;
+                return Ok(res)
+            },
+
+            Some((offset, c)) => {
+                if c.is_whitespace() && !first_char {
+                    start += c.len_utf8();
+                } 
+                else if c.is_whitespace() && first_char {
+                    let res = s[start..*i+offset].to_string();
+                    *i += offset;
+                    return Ok(res);
+                } 
+                else if !c.is_whitespace() {
+                    first_char = true;
+                }
+
+                if c == ',' || c == '}' {
+                    let res = s[start..*i+offset].to_string();
+                    *i += offset;
+                    return Ok(res);
+                }
+            }
+        }
+    }
 }
--- src/derialize/test.rs
+++ src/derialize/test.rs
@@ -1,8 +1,8 @@
-
 use super::{
     ParseError,
     read_string_in_quotes,
-    read_field_name
+    read_field_name,
+    read_string_out_quotes
 };
 
 #[test]
@@ -29,7 +29,7 @@ fn read_string_success3() {
 
     let mut i = 1;
     let _ = read_string_in_quotes(&s1, &mut i);
-    assert_eq!(&s1[i..], "\"Hello");
+    assert_eq!(&s1[i..], "Hello");
 }
 
 #[test]
@@ -65,6 +65,8 @@ fn read_field_name_success2() {
 
     let mut i = 0;
     let name1 = read_field_name(&s, &mut i);
+    assert_eq!(&s[i..i+1], ",");
+    i += 1;
     let name2 = read_field_name(&s, &mut i);
     assert_eq!("Name1", name1.unwrap());
     assert_eq!("Name2", name2.unwrap());
@@ -77,4 +79,75 @@ fn read_field_name_success3() {
     let mut i = 0;
     let _ = read_field_name(&s, &mut i);
     assert_eq!(", \"Name2\"", &s[i..]);
+}
+
+#[test]
+fn read_string_out_quotes_success1() {
+    let s = "false";
+
+    let mut i = 0;
+    let res = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("false")), res);
+}
+
+#[test]
+fn read_string_out_quotes_success2() {
+    let s = "false,";
+
+    let mut i = 0;
+    let res = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("false")), res);
+}
+
+#[test]
+fn read_string_out_quotes_success3() {
+    let s = "   123.5   ";
+
+    let mut i = 0;
+    let res = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("123.5")), res);
+}
+
+#[test]
+fn read_string_out_quotes_success4() {
+    let s = "";
+
+    let mut i = 0;
+    let res = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("")), res);
+}
+
+#[test]
+fn read_string_out_quotes_success5() {
+    let s = "                     ";
+
+    let mut i = 0;
+    let res = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("")), res);
+}
+
+#[test]
+fn read_string_out_quotes_success6() {
+    let s = "   abc   def   ";
+
+    let mut i = 0;
+    let res1 = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("abc")), res1);
+    let res2 = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("def")), res2);
+}
+
+#[test]
+fn read_string_out_quotes_success7() {
+    let s = "abc, def";
+
+    let mut i = 0;
+    let res1 = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("abc")), res1);
+    
+    assert_eq!(",", &s[i..i+1]);
+
+    i += 1;
+    let res2 = read_string_out_quotes(&s, &mut i);
+    assert_eq!(Ok(String::from("def")), res2);
 }
