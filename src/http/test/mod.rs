5324bd905476678cbaeee8bf29759c2e984248cd
RipInPease <snorre.fjelde@gmail.com>
2026-08-04 21:49:42 +0200

    Fuck

--- src/http/test/mod.rs
+++ src/http/test/mod.rs
@@ -51,3 +51,27 @@ fn read_status_1() {
 
     assert_eq!(http_status, should);
 }
+
+#[test]
+fn read_headers_1() {
+    use std::collections::HashMap;
+
+    let mut file = std::fs::File::open("src/http/test/headers.txt").unwrap();
+    let _ = Response::read_version_status(&mut file).unwrap();
+
+    let headers = Response::read_headers(&mut file).unwrap();
+    
+    let should: HashMap<String, String> = HashMap::from([
+        ("Date".to_string(), "Tue, 28 Jul 2026 06:25:38 GMT".to_string()),
+        ("Content-Type".to_string(), "application/json; charset=utf-8".to_string()),
+        ("Transfer-Encoding".to_string(), "chunked".to_string()),
+        ("Connection".to_string(), "close".to_string()),
+        ("access-control-allow-headers".to_string(), "Accept, Accept-Charset, Accept-Language, Authorization, Cache-Control, Content-Language, Content-Type, DNT, Host, If-Modified-Since, Keep-Alive, Origin, Referer, User-Agent, X-Requested-With".to_string()),
+        ("access-control-allow-methods".to_string(), "GET, POST, DELETE, OPTIONS".to_string()),
+        ("access-control-allow-origin".to_string(), "*".to_string()),
+        ("access-control-max-age".to_string(), "300".to_string()),
+        ("Cache-Control".to_string(), "public, max-age=57600".to_string()),
+    ]);
+
+    assert_eq!(headers, should);
+}
